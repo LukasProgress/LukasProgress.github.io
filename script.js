69eb011c052eb780d2e756fee6085fb4c25328f1
Your Name <your.email@example.com>
2025-02-26 17:32:31 +0100

    hobby

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function showEmail() {
     var user = "probst404";
     var domain = "gmail.com";
     var email = user + "@" + domain;
-    document.getElementById("email-link").href = "mailto:" + email;
+    return "mailto:" + email;
 }
